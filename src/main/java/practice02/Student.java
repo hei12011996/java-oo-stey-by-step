36211317eb4cf5e68321507146716b324ee7928f
Antony Chan <antony.chan@oocl.com>
2018-11-14 10:00:27 +0800

    Finish Practice 02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private Integer klass;
+
+    public Student(String name, Integer age, Integer klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass(){
+        return this.klass;
+    }
+
+    @Override
+    public String introduce(){
+        return String.format("I am a Student. I am at Class %d.", this.getKlass());
+    }
 }
