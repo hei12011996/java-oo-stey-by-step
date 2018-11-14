50242cc0ef1b51320c9a5725cba96a4f6436a129
Antony Chan <antony.chan@oocl.com>
2018-11-14 10:11:32 +0800

    Finish Practice 06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, Integer age, Integer klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, Integer age){
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Integer getKlass(){
+        return this.klass;
+    }
+
+    @Override
+    public String introduce(){
+        if (getKlass() == null) {
+            return super.introduce() + String.format(" I am a Teacher. I teach No Class.");
+        } else {
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", getKlass());
+        }
+    }
 }
