ad344f2029e52b11755eef430ced59b72c423264
Antony Chan <antony.chan@oocl.com>
2018-11-14 10:46:22 +0800

    Finish Practice 09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,38 @@
 package practice09;
 
 public class Klass {
+    private Integer number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(Integer number){
+        this.number = number;
+        this.displayName = String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student leader){
+        if(leader.getKlass().number != this.number){
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = leader;
+        }
+    }
+
+    public void appendMember(Student member){
+        member.setKlass(this);
+    }
+
+    public Student getLeader(){
+        return this.leader;
+    }
+
+    public Integer getNumber(){
+        return this.number;
+    }
+
+    public String getDisplayName(){
+        return this.displayName;
+    }
 }
+
+
